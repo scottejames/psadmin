fc6af451f56ea7e6b09f7dade1ed62bc1ef4ac81
scott james <scottejames@scotts-MacBook-Pro.local>
2016-04-18 09:05:22 +0100

    Adding bootstrap, will have to remove dist/.. later

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,10 @@ var config = {
 		paths : {
 			html : './src/*.html',
 			js: './src/**/*.js',
+			css: [
+      			'node_modules/bootstrap/dist/css/bootstrap.min.css',
+      			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+    			], 
 			dist : './dist/',
 			mainJs: './src/main.js'
 
@@ -53,10 +57,19 @@ gulp.task('js', function() {
 		.pipe(connect.reload());
 });
 
+gulp.task('css', function() {
+	gulp.src(config.paths.css)
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
 gulp.task('watch', function() {
 	gulp.watch(config.paths.html, ['html']);
 	gulp.watch(config.paths.js, ['js']);
 
 });
 
-gulp.task('default', ['html', 'js', 'open','watch']);
+gulp.task('default', ['html', 'js', 'css','open','watch']);
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+$ = jQuery  = Require('jquery')
+
 var App = console.log("Hello World from me!")
 
 module.export  = App
